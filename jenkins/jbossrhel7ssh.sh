9ab2e49e1297e0283012dcf6c0f75a070007ba46
root <root@localhost.localdomain>
2014-11-25 18:47:46 +0000

    Updates to support start/stop and parameter for container name

--- jenkins/jbossrhel7ssh.sh
+++ jenkins/jbossrhel7ssh.sh
@@ -1,5 +1,16 @@
-#Start JBoss RHEL7 Docker Container Build Slave
+#!/bin/bash
+
+# Start/Stop JBoss RHEL7 Docker Container Build Slave
 # Redirect SSH Port to 3322
 # Default Password
 #
-sudo docker run -d -p 0.0.0.0:3322:22 -e ROOT_PASS="mypass" --name jbossrhel7bld-slave1 jbossrhel7bld
+if [ $2 = "start" ]; then
+   echo "Starting Jenkins build slave " 
+   sudo docker rm $1
+   sudo docker run -d -p 0.0.0.0:3322:22 -e ROOT_PASS="mypass" --name=$1 jbossrhel7bld
+fi
+
+if [ $2 = "stop" ]; then
+   echo "Stopping Jenkins build slave "
+   sudo docker stop $1
+fi
