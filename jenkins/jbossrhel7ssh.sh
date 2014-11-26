460a86fe69c488312d9601b369cfd9e1bd8c678d
root <root@localhost.localdomain>
2014-11-26 20:48:46 +0000

    Simplify start/stop slave scripts

--- jenkins/jbossrhel7ssh.sh
+++ jenkins/jbossrhel7ssh.sh
@@ -6,8 +6,7 @@
 #
 if [ $2 = "start" ]; then
    echo "Starting Jenkins build slave " 
-   sudo docker rm $1
-   sudo docker run -d -p 0.0.0.0:3322:22 -e ROOT_PASS="mypass" --name=$1 jbossrhel7bld
+   sudo docker run --rm -d -p 0.0.0.0:3322:22 -e ROOT_PASS="mypass" --name=$1 jbossrhel7bld
 fi
 
 if [ $2 = "stop" ]; then
